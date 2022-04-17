8a1ba1302e2a7e7ef383a21a4cf60adae747a40e
KseniyaSaltik <irbis.mr@gmail.com>
2022-04-17 21:07:39 +0300

    Adding comment form.Edit the views.py file by adding CommentForm form and and modify the post_detail view.

--- blog/blog_api/post/urls.py
+++ blog/blog_api/post/urls.py
@@ -6,4 +6,5 @@ app_name = 'post' #blog
 urlpatterns=[
     path('', views.post_list, name="post_list"),
     path('<slug:post>/', views.post_detail, name="post_detail"),
+    path('comment/reply/', views.reply_page, name="reply"),
 ]
--- blog/blog_api/post/views.py
+++ blog/blog_api/post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -26,5 +27,45 @@ def post_list(request):
 def post_detail(request, post):
     ''' Returns particular published post by it's slug-name. '''
 
-    post = get_object_or_404(Post, slug=post, status='published')
-    return render(request, 'post_detail.html', {'post':post})
+    def post_detail(request, post):
+    post = get_object_or_404(Post,slug=post,status='published')
+    
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            # redirect to same page and focus on that comment
+            return redirect(post.get_absolute_url()+'#'+str(new_comment.id))
+        else:
+            comment_form = CommentForm()
+    return render(request, 'post_detail.html',{'post':post,'comments': comments,'comment_form':comment_form})
+
+# handling reply, reply view
+def reply_page(request):
+    if request.method == "POST":
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            post_id = request.POST.get('post_id') 
+            parent_id = request.POST.get('parent') 
+            post_url = request.POST.get('post_url')
+            
+            reply = form.save(commit=False)
+    
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+            
+            return redirect(post_url+'#'+str(reply.id))
+    
+    return redirect("/")
