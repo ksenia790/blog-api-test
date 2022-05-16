def1b10308b228ec4f8997ee47fb696e02f8be61
Sena <77285836+ksenia790@users.noreply.github.com>
2022-05-16 10:02:00 +0300

    Correcting spaces

--- blog/blog_api/post/views.py
+++ blog/blog_api/post/views.py
@@ -3,7 +3,6 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import CommentForm
 
-# Create your views here.
 
 def post_list(request):
     '''
@@ -11,21 +10,17 @@ def post_list(request):
     '''
 
     posts = Post.published.all()
-
     paginator = Paginator(posts, 10)
-
     page = request.GET.get('page')
 
     try:
         posts = paginator.page(page)
 
     except PageNotAnInteger:
-
         # If page is not an integer deliver the first page
         posts = paginator.page(1)
 
     except EmptyPage:
-
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
         
@@ -38,29 +33,17 @@ def post_detail(request, post):
     '''
 
     post = get_object_or_404(Post,slug=post,status='published')
-    
     # List of active comments for particular post
     comments = post.comments.filter(active=True)
     new_comment = None
     comment_form = CommentForm(data=request.POST) 
-
     if request.method == 'POST':
-
         # A comment was posted
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
-
-            # Create Comment object but don't save to database yet.
             new_comment = comment_form.save(commit=False)
-
-            # Assign the current post to the comment.
             new_comment.post = post
-
-            # Save the comment to the database.
             new_comment.save()
-
-            # Redirect to same page and focus on that comment.
             return redirect(post.get_absolute_url()+'#'+str(new_comment.id))
         else:
             comment_form = CommentForm()
@@ -70,21 +53,15 @@ def post_detail(request, post):
 # Handling reply, reply view.
 def reply_page(request):
     if request.method == "POST":
-
         form = CommentForm(request.POST)
-
         if form.is_valid():
             post_id = request.POST.get('post_id') 
             parent_id = request.POST.get('parent') 
             post_url = request.POST.get('post_url')
-            
             reply = form.save(commit=False)
-    
             reply.post = Post(id=post_id)
             reply.parent = Comment(id=parent_id)
             reply.children = Comment(id=parent_id)
             reply.save()
-            
             return redirect(post_url+'#'+str(reply.id))
-    
-    return redirect("/")
+     return redirect("/")
